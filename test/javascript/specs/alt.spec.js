b686f3d53e38318df2100f1ab671e3230647566c
Devin Weaver <suki@tritarget.org>
2013-02-06 15:53:17 -0500

    Sets up initial testing environment

---
+++ test/javascript/specs/alt.spec.js
@@ -0,0 +1,7 @@
+(function() {
+	describe("test", function() {
+		it("should pass", function() {
+			expect(true).toBeTruthy();
+		});
+	});	
+})();
---
+++ test/javascript/specs/main.spec.js
@@ -0,0 +1,10 @@
+describe("jchat", function() {
+	describe("class Settings", function() {
+		describe("#reset", function() {
+		});
+		describe("#set", function() {
+		});	
+		describe("#get", function() {
+		});	
+	});	
+});	
